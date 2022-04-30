92e76a4f961df5b92cf6dd1b760b59397c2bf3dd
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-29 19:14:44 -0800

    doom-hacky build script, correct self-hosted streaming service name

--- build.sh
+++ build.sh
@@ -1,2 +1,43 @@
 #!/bin/sh
-emacs -Q --script build-site.el
+":"; exec emacs -Q --script "$0" "$@" # -*- mode: emacs-lisp; -*-
+(require 'ox-publish)
+
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+(setq org-publish-project-alist
+      (list
+       (list "my-org-site"
+             :recursive t
+             :base-directory "./org"
+             :publishing-directory "./html"
+             :publishing-function 'org-html-publish-to-html
+	     :section-numbers nil
+	     :with-creator t
+	     :with-author nil
+	     :with-toc nil
+	     )))
+
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style t ;nil ;; Use our own styles
+      ;; org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+      org-html-head "<link rel=\"stylesheet\" href=\"/style.css\" />"
+      )
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build Complete!")
