b90480f6c5ecac252d0af2f4042865a5876aa2db
MitchMarq42 <dr.m.perseos@gmail.com>
2022-08-14 11:11:14 -0800

    Add dir-locals, to confuse emacs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((org-hugo-base-dir . "content/org")))
+ ("content-org/" . ((org-mode . ((eval org-hugo-auto-export-mode))))))
